2fbab055243d1cda04852cc6e6c01d126e4c78aa
Pawel Gebal <pawel.gebal@imapp.pl>
2023-07-05 20:36:16 +0200

    refactor BECToken.sol

--- solidity_examples/BECToken.sol
+++ solidity_examples/BECToken.sol
@@ -1,43 +1,14 @@
-
-/**
- * @title SafeMath
- * @dev Math operations with safety checks that throw on error
- */
-library SafeMath {
-  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-    uint256 c = a * b;
-    assert(a == 0 || c / a == b);
-    return c;
-  }
-
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return c;
-  }
-
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
-
-  function add(uint256 a, uint256 b) internal pure returns (uint256) {
-    uint256 c = a + b;
-    assert(c >= a);
-    return c;
-  }
-}
+pragma solidity >=0.0.0;
 
 /**
  * @title ERC20Basic
  * @dev Simpler version of ERC20 interface
  * @dev see https://github.com/ethereum/EIPs/issues/179
  */
-contract ERC20Basic {
+abstract contract ERC20Basic {
   uint256 public totalSupply;
-  function balanceOf(address who) public returns (uint256);
-  function transfer(address to, uint256 value) public returns (bool);
+  function balanceOf(address who) public virtual returns (uint256);
+  function transfer(address to, uint256 value) public virtual returns (bool);
   event Transfer(address indexed from, address indexed to, uint256 value);
 }
 
@@ -46,7 +17,6 @@ contract ERC20Basic {
  * @dev Basic version of StandardToken, with no allowances.
  */
 contract BasicToken is ERC20Basic {
-  using SafeMath for uint256;
 
   mapping(address => uint256) balances;
 
@@ -55,23 +25,18 @@ contract BasicToken is ERC20Basic {
   * @param _to The address to transfer to.
   * @param _value The amount to be transferred.
   */
-  function transfer(address _to, uint256 _value) public returns (bool) {
+  function transfer(address _to, uint256 _value) public override virtual returns (bool) {
     require(_to != address(0));
     require(_value > 0 && _value <= balances[msg.sender]);
 
     // SafeMath.sub will throw if there is not enough balance.
-    balances[msg.sender] = balances[msg.sender].sub(_value);
-    balances[_to] = balances[_to].add(_value);
+    balances[msg.sender] = balances[msg.sender] - _value;
+    balances[_to] = balances[_to] + _value;
     emit Transfer(msg.sender, _to, _value);
     return true;
   }
 
-  /**
-  * @dev Gets the balance of the specified address.
-  * @param _owner The address to query the the balance of.
-  * @return An uint256 representing the amount owned by the passed address.
-  */
-  function balanceOf(address _owner) public returns (uint256 balance) {
+  function balanceOf(address _owner) public override returns (uint256 balance) {
     return balances[_owner];
   }
 }
@@ -80,10 +45,10 @@ contract BasicToken is ERC20Basic {
  * @title ERC20 interface
  * @dev see https://github.com/ethereum/EIPs/issues/20
  */
-contract ERC20 is ERC20Basic {
-  function allowance(address owner, address spender) public returns (uint256);
-  function transferFrom(address from, address to, uint256 value) public returns (bool);
-  function approve(address spender, uint256 value) public returns (bool);
+abstract contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender) public virtual returns (uint256);
+  function transferFrom(address from, address to, uint256 value) public virtual returns (bool);
+  function approve(address spender, uint256 value) public virtual returns (bool);
   event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
@@ -106,14 +71,14 @@ contract StandardToken is ERC20, BasicToken {
    * @param _to address The address which you want to transfer to
    * @param _value uint256 the amount of tokens to be transferred
    */
-  function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+  function transferFrom(address _from, address _to, uint256 _value) public override virtual returns (bool) {
     require(_to != address(0));
     require(_value > 0 && _value <= balances[_from]);
     require(_value <= allowed[_from][msg.sender]);
 
-    balances[_from] = balances[_from].sub(_value);
-    balances[_to] = balances[_to].add(_value);
-    allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+    balances[_from] = balances[_from] - _value;
+    balances[_to] = balances[_to] + _value;
+    allowed[_from][msg.sender] = allowed[_from][msg.sender] - _value;
     emit Transfer(_from, _to, _value);
     return true;
   }
@@ -128,19 +93,13 @@ contract StandardToken is ERC20, BasicToken {
    * @param _spender The address which will spend the funds.
    * @param _value The amount of tokens to be spent.
    */
-  function approve(address _spender, uint256 _value) public returns (bool) {
+  function approve(address _spender, uint256 _value) public override virtual returns (bool) {
     allowed[msg.sender][_spender] = _value;
     emit Approval(msg.sender, _spender, _value);
     return true;
   }
 
-  /**
-   * @dev Function to check the amount of tokens that an owner allowed to a spender.
-   * @param _owner address The address which owns the funds.
-   * @param _spender address The address which will spend the funds.
-   * @return A uint256 specifying the amount of tokens still available for the spender.
-   */
-  function allowance(address _owner, address _spender) public returns (uint256 remaining) {
+  function allowance(address _owner, address _spender) public override returns (uint256 remaining) {
     return allowed[_owner][_spender];
   }
 }
@@ -239,15 +198,15 @@ contract Pausable is Ownable {
 
 contract PausableToken is StandardToken, Pausable {
 
-  function transfer(address _to, uint256 _value) public whenNotPaused returns (bool) {
+  function transfer(address _to, uint256 _value) public override(ERC20Basic, BasicToken) whenNotPaused returns (bool) {
     return super.transfer(_to, _value);
   }
 
-  function transferFrom(address _from, address _to, uint256 _value) public whenNotPaused returns (bool) {
+  function transferFrom(address _from, address _to, uint256 _value) public override whenNotPaused returns (bool) {
     return super.transferFrom(_from, _to, _value);
   }
 
-  function approve(address _spender, uint256 _value) public whenNotPaused returns (bool) {
+  function approve(address _spender, uint256 _value) public override whenNotPaused returns (bool) {
     return super.approve(_spender, _value);
   }
 
@@ -257,9 +216,9 @@ contract PausableToken is StandardToken, Pausable {
     require(cnt > 0 && cnt <= 20);
     require(_value > 0 && balances[msg.sender] >= amount);
 
-    balances[msg.sender] = balances[msg.sender].sub(amount);
+    balances[msg.sender] = balances[msg.sender] - amount;
     for (uint i = 0; i < cnt; i++) {
-        balances[_receivers[i]] = balances[_receivers[i]].add(_value);
+        balances[_receivers[i]] = balances[_receivers[i]] + _value;
         emit Transfer(msg.sender, _receivers[i], _value);
     }
     return true;
@@ -289,10 +248,5 @@ contract BecToken is PausableToken {
     constructor() public {
       totalSupply = 7000000000 * (10**(uint256(decimals)));
       balances[msg.sender] = totalSupply;    // Give the creator all initial tokens
-    }
-
-    function () external {
-        //if ether is sent to this address, send it back.
-        revert();
     }
 }
